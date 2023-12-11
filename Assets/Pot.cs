c6dd0f2681765b11ba60b6d3c535792cb55fe33d
pijussJ <123236501+pijussJ@users.noreply.github.com>
2023-12-11 21:41:12 +0200

    VFX

--- Assets/Pot.cs
+++ Assets/Pot.cs
@@ -7,11 +7,15 @@ public class Pot : MonoBehaviour
     public float shrinkSpeed = 5;
     public Vector3 rotateSpeed;
     AudioSource source;
+    public GameObject pot;
     private void OnMouseDown()
     {
         GameManager.clicks++;
         transform.localScale = Vector3.one * 1.5f;
         source.Play();
+        float randomX = Random.Range(-0.8f, 0.8f);
+        Vector3 spawnposition = new Vector3(randomX, 5f, 0f);
+        Instantiate(pot, spawnposition, Quaternion.identity);
     }
     private void Start()
     {
